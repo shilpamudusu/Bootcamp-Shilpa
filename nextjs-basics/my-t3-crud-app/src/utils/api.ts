3471613038e93e36b3381c460b91e82ead4a915e
shilpamudusu <mudusushilpa@gmail.com>
2025-01-16 15:08:08 +0530

    completed

--- nextjs-basics/my-t3-crud-app/src/utils/api.ts
+++
@@ -1,4 +0,0 @@
-import { createTRPCReact } from '@trpc/react-query';
-import { AppRouter } from '../server/api/root';  // Update this path based on where your router file is
-
-export const api = createTRPCReact<AppRouter>();
